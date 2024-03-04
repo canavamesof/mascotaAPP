a8045004623fb124528d8726ecb72e2448dde834
Carolina Navarro <caromoramanager@gmail.com>
2024-03-04 11:05:33 -0600

    trate de resolver la conexion entre read y update, cambio de boton ingreso de informacion

--- src/views/ReadView.jsx
+++ src/views/ReadView.jsx
@@ -6,20 +6,18 @@ const ReadView = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    getUsers();// Traer los usuarios cuando el componente se monto
+    getUsers();
   }, []);
 
   const getUsers = async () => {
     const response = await userServices.getUsers();
-
     setUsers(response.data);
   };
 
   const deleteUser = async (id) => {
     const response = await userServices.deleteUser(id);
-
     if (response.data.id) {
-      getUsers();
+      getUsers(); // Actualizar lista de usuarios después de eliminar uno
     }
   };
 
@@ -35,36 +33,9 @@ const ReadView = () => {
         >
           Usuarios
         </label>
-        <button
-          className="btn btn-primary"
-          style={{
-            borderRadius: "50%",
-            height: "55px",
-            width: "55px",
-          }}
-          onClick={() => {
-            window.location.href = "/create";
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-plus-lg"
-            viewBox="0 0 16 16"
-            style={{
-              color: "white",
-              fontSize: "30px",
-              fontWeight: "bold",
-            }}
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
-            />
-          </svg>
-        </button>
+        <Link to="/create" className="btn btn-primary">
+          Ingresar informacion
+        </Link>
       </div>
       <table className="table table-striped">
         <thead>
@@ -91,13 +62,6 @@ const ReadView = () => {
                 <td>{data.NombreMascota}</td>
                 <td>{data.Tipo}</td>
                 <td>{data.UltimaVisita}</td>
-                <td
-                  onClick={() => {
-                    window.location.href = `mailto:${data.email}`;
-                  }}
-                >
-                  <label className="item-select">{data.email}</label>
-                </td>
                 <td>
                   <Link
                     to={{
@@ -105,25 +69,7 @@ const ReadView = () => {
                     }}
                     className="btn btn-warning"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-arrow-clockwise"
-                      viewBox="0 0 16 16"
-                      style={{
-                        color: "white",
-                        fontSize: "20px",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"
-                      />
-                      <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466" />
-                    </svg>
+                    Update
                   </Link>
                 </td>
                 <td>
@@ -133,22 +79,7 @@ const ReadView = () => {
                       deleteUser(data.id);
                     }}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-trash"
-                      viewBox="0 0 16 16"
-                      style={{
-                        color: "white",
-                        fontSize: "20px",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                    </svg>
+                    Delete
                   </button>
                 </td>
               </tr>
--- src/views/UpdateView.jsx
+++ src/views/UpdateView.jsx
@@ -1,6 +1,7 @@
+
 import { useParams } from "react-router-dom";
-import userServices from "../services/userServices";
 import { useEffect, useState } from "react";
+import userServices from "../services/userServices";
 import sourceShare from "../share/sourceShare";
 
 const UpdateView = () => {
@@ -32,8 +33,7 @@ const UpdateView = () => {
   };
 
   const updateUser = async () => {
-    // Validar el correo electrónico si lo obtienes de alguna parte
-      if (sourceShare.esValidemail()) {
+    if (sourceShare.esValidEmail()) {
       let updateUser = {
         nombreDueño: nombreDueño,
         Telefono: Telefono,
@@ -44,15 +44,12 @@ const UpdateView = () => {
 
       const response = await userServices.updateUser(updateUser, id);
 
-      console.log(response);
-
       if (response.data.id) {
-        window.location.href = "/read";
+        history.push("/read");
       }
-} else {
-    //   //Notificar al usuario que el correo es inválido
- setTextAlert("El correo no cumple con el formato");
- }
+    } else {
+      setTextAlert("El correo no cumple con el formato");
+    }
   };
 
   return (
@@ -66,61 +63,7 @@ const UpdateView = () => {
       )}
 
       <form>
-        <div className="mb-3">
-          <label className="form-label">Nombre Dueño</label>
-          <input
-            value={nombreDueño}
-            onChange={(event) => setNombreDueño(event.target.value)}
-            type="text"
-            className="form-control"
-            id="nombreDueño"
-          />
-        </div>
-
-        <div className="mb-3">
-          <label className="form-label">Telefono</label>
-          <input
-            value={Telefono}
-            onChange={(event) => setTelefono(event.target.value)}
-            type="text"
-            className="form-control"
-            id="Telefono"
-          />
-        </div>
-
-        <div className="mb-3">
-          <label className="form-label">Nombre Mascota</label>
-          <input
-            value={NombreMascota}
-            onChange={(event) => setNombreMascota(event.target.value)}
-            type="text"
-            className="form-control"
-            id="NombreMascota"
-          />
-        </div>
-
-        <div className="mb-3">
-          <label className="form-label">Tipo</label>
-          <input
-            value={Tipo}
-            onChange={(event) => setTipo(event.target.value)}
-            type="text"
-            className="form-control"
-            id="Tipo"
-          />
-        </div>
-
-        <div className="mb-3">
-          <label className="form-label">Fecha Ultima Visita</label>
-          <input
-            value={FechaUltimaVisita}
-            onChange={(event) => setFechaUltimaVisita(event.target.value)}
-            type="text"
-            className="form-control"
-            id="FechaUltimaVisita"
-          />
-        </div>
-
+        {/* Aquí van los campos de actualización del usuario */}
         <button
           className="btn btn-info"
           onClick={() => {
@@ -136,4 +79,3 @@ const UpdateView = () => {
 };
 
 export default UpdateView;
-
